61098d0bd229f7cde16440346d8f1b9b8ccae23c
Trevor Oakes <toakes8@ivytech.edu>
2022-11-02 05:14:12 -0400

    My Django Girls app, first commit

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+]
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.post_list, name='post_list'),
 ]
